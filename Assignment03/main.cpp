cb8428ea441eb2ae89df830f05d96cc20f5d0bee
Ben Visness <bvisness@gmail.com>
2015-10-01 16:40:00 -0500

    Initial Commit

---
+++ Assignment03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment03
+//
+//  Created by Benjamin Visness on 10/1/15.
+//  Copyright (c) 2015 Benjamin Visness. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
